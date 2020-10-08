23ecda1b4f98240c20a46dd6094e141876cc80fa
Hugh Osborne <h.r.osborne@hud.ac.uk>
2020-10-08 17:47:22 +0100

    Initial commit

---
+++ src/main/java/genericMethods/Swap.java
@@ -0,0 +1,19 @@
+package genericMethods;
+
+/**
+ * A "stub" class for a class that should provide a generic swap method.
+ *
+ * @author Hugh Osborne
+ * @version October 2020
+ */
+
+public class Swap {
+    /**
+     * Swap two entries in an array.
+     * @param array the array in which entries are to be swapped.
+     * @param index1 the index of the first entry to be swapped.
+     * @param index2 the index of the second entry to be swapped.
+     * @param <T> the type of objects in the array.
+     * @throws ArrayIndexOutOfBoundsException if either of the indices is out of bounds.
+     */
+}
